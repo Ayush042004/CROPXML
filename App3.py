758c30a23e0da4cebf2b97eabc3e0ddee06cd5f4
Ayush kumar <kumarayush042004@gmail.com>
2025-03-24 19:12:51 +0530

    ports

--- App3.py
+++ App3.py
@@ -61,7 +61,7 @@ def get_farming_alert():
     return jsonify({"alerts": farming_alerts})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
+    port = int(os.environ.get("PORT", 10002)) 
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
--- Rnn/App2.py
+++ Rnn/App2.py
@@ -75,5 +75,5 @@ def get_disease_info():
         return jsonify({"error": "Disease not found"}), 404
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port , debug=True)
